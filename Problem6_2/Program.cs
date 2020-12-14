71a1fc149d48976b785ef00ad6c909d08d4ff2da
Neeraj Pandey <45538973+neerajp67@users.noreply.github.com>
2020-12-14 18:11:50 +0530

    Add files via upload

---
+++ Problem6_2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, arrCount;
+            Console.WriteLine("Enter Length of Array: ");
+            arrCount = int.Parse(Console.ReadLine());
+            string[] arr = new string[arrCount];
+
+            //taking user i/p
+            Console.WriteLine("Enter Data in Array: ");
+            for(i=0; i<= arrCount-1; i++)
+            {
+                Console.WriteLine("String {0} : ", i);
+                arr[i] = Console.ReadLine();
+            }
+
+            //Calling SortLexicalOrder to sort Strings in Lexical Order
+            SortLexicalOrder(arr);
+
+            //Calling SortString for sorting string length wise
+            SortStringLength(arr, arrCount);
+
+            Console.WriteLine("Printing Sorted Array-");
+            PrintArray(arr, arrCount);
+
+            //sorting string in lexical order
+            void SortLexicalOrder(string[] str)
+            {
+                //Array.Sort(str);
+                for (i = 0; i < arrCount; i++)
+                {
+                    for (int j = 0; j < arrCount - 1; j++)
+                    {
+                        if (arr[j].CompareTo(arr[j + 1]) > 0)
+                        {
+                            string temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+
+            //sorting string length wise
+            void SortStringLength(string []s, int n)
+            {
+                for (i = 1; i < n; i++)
+                {
+                    string tempString = s[i];
+
+                    int j = i - 1;
+                    try { 
+                    while(j>=0 && tempString.Length < s[j].Length)
+                    {
+                        s[j + 1] = s[j];
+                        j--;
+                            
+                    }
+                    s[j+1] = tempString;
+
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Error: " + e);
+                    }
+                }
+            }
+
+            //display sorted array data
+            void PrintArray(String[] str, int n)
+            {
+                for (i = 0; i <= n - 1; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Problem6_3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Problem6_3
+{
+
+    public class Products
+    {
+        public int productId { get; set; }
+        public string brandName { get; set; }
+        public string description { get; set; }
+        public float price { get; set; }
+    }
+    class Product
+    {
+        static void Main(string[] args)
+        {
+            List<Products> product = new List<Products>() {
+            new Products() { productId = 200, brandName = "Dell", description = "15 inch Monitor", price = 3400.44f },
+            new Products() { productId = 120, brandName = "Dell", description = "Laptop", price = 45000.00f },
+           new Products() { productId = 150, brandName = "Microsoft", description = "Windows 7", price = 7000.50f },
+           new Products() { productId = 100, brandName = "Logitech", description = "Opetical Mouse", price = 540.00f },
+            };
+            Console.WriteLine("Product ID\t Brand Name\t\tDescription\t\t\t Price");
+            foreach (Products prod in product)
+                Console.WriteLine(prod.productId + "\t\t " + prod.brandName + "\t\t\t"+ prod.description+ "\t\t\t    "+prod.price);
+            Console.ReadLine();
+            OrderById(product, 4);
+
+
+            foreach (Products prod in product)
+                Console.WriteLine(prod.productId + "\t\t " + prod.brandName + "\t\t\t" + prod.description + "\t\t\t    " + prod.price);
+            Console.ReadLine();
+        }
+        static void OrderById(List<Products> lst, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i+1; j < n; j++)
+                {
+                    if (lst[i].productId >lst[j].productId)
+                    {
+                        var temp = lst[i];
+                        lst[i] = lst[j];
+                        lst[j ] = temp;
+                    }
+                }
+            }
+            
+        }
+    }
+
+
+}
+        
+    
+
